0f4142bdcee23831d9d8d4878b367b708b22174f
HoangQuan425 <quanqhhq@gmail.com>
2024-12-10 17:48:52 +0700

    add authentication login and js of login

---
+++ MyProject.Web/Areas/Admin/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyProject.Web.Areas.Admin.Controllers
+{
+  public class AuthenticationController : Controller
+  {
+    public IActionResult Login()
+    {
+      return View();
+    }
+  }
+}
---
+++ MyProject.Web/wwwroot/app/controllers/login/index.js
@@ -0,0 +1,31 @@
+﻿var loginController = function () {
+	this.initialize = function () {
+
+	}
+	var registerEvents = function () {
+		$('#btnLogin'), on('click', function (e) {
+			e.preventDefault();
+			var user = $('#userName').val();
+			var pass = $('#password').val();
+			login(user, pass);
+		});
+	},
+	var login = function (user, pass) {
+		$.ajax({
+			type: "post",
+			data: {
+				userName: user,
+				password: pass
+			},
+			dataType: 'json',
+			url: '/admin/login/authen',
+			success: function (res) {
+				if (res.success) {
+					window.location.href = "/Admin/Home/Login";
+				} else {
+					myWebsite.notify('Login fail!');
+				}
+			}
+		})
+	}
+}
